a34430fef52d4be09f26f4494bf678d3a711920e
Crasasito <50593926+Crasasito@users.noreply.github.com>
2025-11-06 01:11:25 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Config real provista por Antonio
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2HaGFibNh-nHeIX2i2mIQ5h2iGaeWsco",
+  authDomain: "planta-59ff8.firebaseapp.com",
+  projectId: "planta-59ff8",
+  storageBucket: "planta-59ff8.firebasestorage.app", // correcto para Storage; no afecta Auth/Firestore
+  messagingSenderId: "185987714097",
+  appId: "1:185987714097:web:024cc87eb8372c52345910"
+};
