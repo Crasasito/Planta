4178e7400da7f79e297a92c1d260b7163af813b5
Crasasito <50593926+Crasasito@users.noreply.github.com>
2025-11-06 23:05:43 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js — planta-59ff8
-export const firebaseConfig = {
-  apiKey: "AIzaSyA2HaGFibNh-nHeIX2i2mIQ5h2iGaeWsco",
-  authDomain: "planta-59ff8.firebaseapp.com",
-  projectId: "planta-59ff8",
-  storageBucket: "planta-59ff8.firebasestorage.app",
-  messagingSenderId: "185987714097",
-  appId: "1:185987714097:web:024cc87eb8372c52345910"
-};
