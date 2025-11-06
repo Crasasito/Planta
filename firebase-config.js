aa46a51bfe08a11b0105feb2d9767023abde9407
Crasasito <50593926+Crasasito@users.noreply.github.com>
2025-11-06 02:01:42 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2HaGFibNh-nHeIX2i2mIQ5h2iGaeWsco",
+  authDomain: "planta-59ff8.firebaseapp.com",
+  projectId: "planta-59ff8",
+  storageBucket: "planta-59ff8.appspot.com",
+  messagingSenderId: "185987714097",
+  appId: "1:185987714097:web:024cc87eb8372c52345910"
+};
