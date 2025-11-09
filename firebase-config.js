e860739941dce7c9549c18f56b4e350ead4a144a
Crasasito <50593926+Crasasito@users.noreply.github.com>
2025-11-09 23:59:40 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA2HaGFibNh-nHeIX2i2mIQ5h2iGaeWsco",
+  authDomain: "planta-59ff8.firebaseapp.com",
+  projectId: "planta-59ff8",
+  storageBucket: "planta-59ff8.firebasestorage.app",
+  messagingSenderId: "185987714097",
+  appId: "1:185987714097:web:024cc87eb8372c52345910"
 };
